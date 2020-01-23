9275a10a9a182f5f23ee1edfa2c3157514e9e290
unknown <picchumacha@gmail.com>
2020-01-24 00:06:14 +0300

    GraphQL-2

---
+++ src/main/java/com/techprimers/graphql/springbootgrapqlexample/resource/BookResource.java
@@ -0,0 +1,25 @@
+package com.techprimers.graphql.springbootgrapqlexample.resource;
+
+import com.techprimers.graphql.springbootgrapqlexample.service.GraphQLService;
+import graphql.ExecutionResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class BookResource {
+
+    @Autowired
+    GraphQLService graphQLService;
+
+    @PostMapping("/graphql")
+    public ResponseEntity<Object> getAllBooks(@RequestBody String query) {
+        ExecutionResult execute = graphQLService.getGraphQL().execute(query);
+
+        return new ResponseEntity<>(execute, HttpStatus.OK);
+    }
+}
